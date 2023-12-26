33ce8966c8558f5f5869420130e44448c18ee47f
Pengcheng Xu <px6@illinois.edu>
2023-12-25 21:11:33 -0600

    Update test_a_cross_n2d.py

--- tests/test_a_cross_n2d.py
+++ tests/test_a_cross_n2d.py
@@ -55,6 +55,31 @@ class NeighborhoodCrossAttention2DTests(unittest.TestCase):
         self.assertIsNotNone(x_q.grad)
         self.assertIsNotNone(x_kv.grad)
 
+    def test_cross_attention_equivalence(self):
+        """
+        Test that NeighborhoodCrossAttention2D with (a, a) has the same output as NeighborhoodAttention2D with (a).
+        """
+        # Define the module parameters
+        dim = 64
+        num_heads = 8
+        kernel_size = 3
+        dilation = 1
+
+        # Create instances of the modules
+        na = NeighborhoodAttention2D(dim, num_heads, kernel_size, dilation)
+        nca = NeighborhoodCrossAttention2D(dim, num_heads, kernel_size, dilation)
+
+        # Create a dummy input
+        B, H, W, C = 2, 16, 16, 64
+        x = torch.randn(B, H, W, C)
+
+        # Forward pass for both modules
+        output_na = na(x)
+        output_nca = nca(x, x)
+
+        # Check if outputs are close enough
+        torch.testing.assert_close(output_na, output_nca, rtol=1e-5, atol=1e-5)
+
 # Additional tests can be added here for different configurations, input shapes, etc.
 
 if __name__ == "__main__":
